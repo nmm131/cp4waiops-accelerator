481c07411e269fa51d14e01702b760728f64932e
Nate <Nate.Malone@ibm.com>
2021-12-10 11:00:10 -0500

    Updated script by naming container that is being run and removing existing container with that name

--- build-container.sh
+++ build-container.sh
@@ -3,5 +3,8 @@
 # Build the container
 docker build -t cp4waiops-accelerator:latest -f ./config/Dockerfile .
 
+# Remove the container
+docker rm cp4waiops-accelerator
+
 # Run the container
-docker run -it --env-file ./config/.env cp4waiops-accelerator bash
+docker run -it --name cp4waiops-accelerator --env-file ./config/.env cp4waiops-accelerator bash

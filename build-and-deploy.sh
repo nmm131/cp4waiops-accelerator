7a426448cf30c6bb3c6574520c8023621e454bbe
Nate <Nate.Malone@ibm.com>
2021-12-14 12:05:54 -0500

    Rename ./build-container.sh to ./build-and-deploy.sh

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -7,4 +7,6 @@ docker build -t cp4waiops-accelerator:latest -f ./config/Dockerfile .
 docker rm cp4waiops-accelerator
 
 # Run the container
-docker run -it --name cp4waiops-accelerator --env-file ./config/.env cp4waiops-accelerator bash
+docker run -it --name cp4waiops-accelerator --env-file ./config/.env cp4waiops-accelerator
+
+docker exec -it ./10_install_ai_manager.sh -t $IBM_ENTITLEMENT_KEY bash

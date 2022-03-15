37b3d8d123848bf09c2960798f8d682f7b8f01fd
Nate <Nate.Malone@ibm.com>
2022-03-15 11:35:22 -0400

    Update install-ai-manager.sh

--- install-ai-manager.sh
+++ install-ai-manager.sh
@@ -1,15 +1,49 @@
 #!/bin/sh
 
-# Check dependencies
-curl -sfL https://raw.github.ibm.com/CP4WAIOPS-Accelerator/cp4waiops-accelerator/main/config/check-dependencies.sh | sh -
+# # Check dependencies
+# curl -sfL https://raw.github.ibm.com/CP4WAIOPS-Accelerator/cp4waiops-accelerator/main/config/check-dependencies.sh | sh -
+# Check if the correct version of jq is installed
+if [ "$(printf "%s\n" "$(jq --version | grep -o '[0-9]\+[.]*[0-9]*')>1.4" | bc)" -eq "0" ]
+then
+	printf "This script uses jq. To run the script, you must install jq\n"
+	exit 1
+fi
 
-# Import variables
-curl -s https://raw.github.ibm.com/CP4WAIOPS-Accelerator/cp4waiops-accelerator/main/config/config.sh | sh -
+# Check if the correct version of OpenShift CLI is installed
+OC_CLIENT_VERSION=$(oc version --client | grep -o '[0-9]\+[.]*[0-9]*' | head -n 1)
+if [ "$(printf "%s\n" "$OC_CLIENT_VERSION>4.5" | bc)" -eq "0" ]
+then
+	oc version
+	printf "This script uses oc. To run the script, you must install OpenShift Client Version >=4.6\n"
+	exit 1
+fi
+
+# Check if logged into OpenShift Cluster
+if [ ! "$(oc status 2> /dev/null)" ]
+then
+	printf "This script requires you to be logged in to an OpenShift cluster. To run the script, you must log in to your OpenShift cluster:\noc login\n"
+	exit 1
+fi
+
+# Check if the correct version of Red Hat OpenShift Container Platform is installed
+OC_SERVER_VERSION=$(oc version -o yaml | grep openshiftVersion | grep -o '[0-9]\+[.]*[0-9]*' | head -n 1)
+if [ "$(printf "%s\n" "$OC_SERVER_VERSION>4.5" | bc)" -eq "0" ]
+then
+	oc version
+	printf "This script uses oc. To run the script, you must install OpenShift Server Version >=4.6\n"
+	exit 1
+fi
+
+# # Import variables
+# curl -s https://raw.github.ibm.com/CP4WAIOPS-Accelerator/cp4waiops-accelerator/main/config/config.sh | sh -
+# TODO: Read input 
 
 # Initialize variables for reading
 # Make sure secret variables are cleared and not exported
 IFS=
 REPLY='continue'
+NAMESPACE=cp4waiops-accelerator
+ENTITLEMENT=ibm-entitlement-key
 set +o allexport
 unset -v REGISTRY_SERVER_ADDRESS
 unset -v USERNAME
